c157357ac171fb4042432055af37e92fdb3c2282
Adarsh <adarshumesh5@gmail.com>
2024-09-15 22:50:04 -0700

    Neon Number

---
+++ NeonNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NeonNumber {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the number to check if it is a neon number or not");
+        int num = myObj.nextInt();
+        int sum=0;
+        int square = num * num ;
+        while(square!=0){
+            int temp = square % 10;
+            sum = sum + temp;
+            square = square/10;
+        }
+       
+        if(num == sum){
+            System.out.println("It is a neon number");
+        }
+        else{
+            System.out.println("It is not a neon number ");
+        }
+    }
+}
